5840acd49d5dc2ba022096269f31b847725d2bdf
Ankit Dhotre <ankitdhotre21@gmail.com>
2026-03-06 15:42:05 +0530

    Fix : CSS

--- portfolio/next.config.ts
+++ portfolio/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/ankkrypt/portfolio' : '',
+  basePath: '/portfolio',
   images: {
     unoptimized: true,
   },
